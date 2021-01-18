d7d69f5639ef631e61fe4800dca4467e1ca39ed0
おーもと <vitamin29290209@gmail.com>
2021-01-18 23:28:52 +0900

    commonのインデント調整

--- laravel/resources/views/common.blade.php
+++ laravel/resources/views/common.blade.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <!-- CSS -->
-  <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-  <title>@yield('title')</title>
-</head>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSS -->
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <title>@yield('title')</title>
+  </head>
 
-
-  <header class="header">
+    <header class="header">
     @include('layouts.header')
-  </header>
-  <main>
-    <div>
-    @yield('content')
-    </div>
-  </main>
-</body>
+    </header>
+
+    <main>
+      <div>
+      @yield('content')
+      </div>
+    </main>
+
+  </body>
 </html>
