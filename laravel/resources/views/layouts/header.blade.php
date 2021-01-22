15c80fdc4325de55da26d6e4588ec05588142df6
おーもと <vitamin29290209@gmail.com>
2021-01-22 21:21:46 +0900

    headerにユーザー名を表示

--- laravel/resources/views/layouts/header.blade.php
+++ laravel/resources/views/layouts/header.blade.php
@@ -2,19 +2,18 @@
 @if (Route::has('login'))
 <div class="header__right">
 @auth
-  <a href="{{ url('/myPage') }}">マイページ</a>
-  <!-- <a href="{{ route('logout') }}">ログアウト</a> -->
+  <a href="{{ url('/myPage') }}" class="header__right__list">{{ Auth::user()->name }}さんのマイページ</a>
   <form class="logout" action="{{ route('logout') }}" method="POST">
     @csrf
     <input class="logout__button" type="submit" value='ログアウト'>
   </form>
 @else
-  <a href="{{ route('login') }}">ログイン</a>
+  <a href="{{ route('login') }}" class="header__right__list">ログイン</a>
 
   @if (Route::has('register'))
-  <a href="{{ route('register') }}">ユーザー登録</a>
+  <a href="{{ route('register') }}" class="header__right__list">ユーザー登録</a>
   @endif
 @endauth
 @endif
-  <a href="{{ route('about') }}">サイト詳細</a>
+  <a href="{{ route('about') }}" class="header__right__list">サイト詳細</a>
 </div>
--- laravel/resources/views/page/index.blade.php
+++ laravel/resources/views/page/index.blade.php
@@ -4,11 +4,11 @@
 @section('content')
 <div class="main">
   <div class="card">
-      <div class="card__title"><i class="fas fa-search"></i>好きな車を見つけよう</div>
+      <div class="card__title"><i class="fas fa-search"></i>好きな車を見つける</div>
       <ul class="card__list">
-        <li>特徴を選ぶとあなたにあった車</li>
+        <li>特徴を選んで探せる</li>
         <li>登録車数350台！</li>
-        <li></li>
+        <li>いろんなパターンを試そう</li>
       </ul>
     <div class="button__group">
       <a href="{{ route('main') }}" class="button__group__start">サーチ</a>
@@ -16,11 +16,11 @@
   </div>
 
   <div class="card">
-      <div class="card__title"><i class="fas fa-camera"></i>画像を投稿しよう</div>
+      <div class="card__title"><i class="fas fa-camera"></i>画像を投稿する</div>
       <ul class="card__list">
-        <li>特徴を選ぶとあなたにあった車</li>
-        <li>登録車数350台！</li>
-        <li></li>
+        <li>あなたの画像でアプリを充実</li>
+        <li>撮った画像を共有</li>
+        <li>ナンバープレートは消してから</li>
       </ul>
     <div class="button__group">
       <a href="{{ route('main') }}" class="button__group__start">シェア</a>
