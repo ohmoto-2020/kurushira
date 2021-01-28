fb1de40cdafa5bb58ca7e4d2bdaa77c2f34d654f
おーもと <vitamin29290209@gmail.com>
2021-01-28 20:23:46 +0900

    [add]検索結果が空の場合の処理を追加

--- laravel/resources/views/page/result.blade.php
+++ laravel/resources/views/page/result.blade.php
@@ -3,9 +3,16 @@
 <body class="body">
 @section('content')
 <div class="result" style="color:red;">
-<pre>
-
-  {{ var_dump($match_car)}}
-</pre>
+  @if($match_car->isEmpty())
+    <p class="not-find" style="color: red;">該当する車は見つかりませんでした</p>
+  @else
+    @foreach($match_car as $car)
+    <div class="result__box">
+      <p>車種名:{{ $car['name'] }}</p>
+      <p>メーカー:{{ $car['maker'] }}</p>
+      <p>価格:{{ $car['price'] }}万円</p>
+    </div>
+    @endforeach
+  @endif
 </div>
 @endsection
