4bf7778c9cfb1bce575cffe93f5e6219e52be135
おーもと <vitamin29290209@gmail.com>
2021-01-31 19:45:49 +0900

    [update]ゲストユーザはユーザー情報を変更不可

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -8,18 +8,25 @@ use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
     //userデータの取得
-    public function index() {
-        return view('user.index', ['user' => Auth::user() ]);
-    }
+    // public function index() {
+    //     return view('user.index', ['user' => Auth::user() ]);
+    // }
+
     //userデータの編集
     public function edit() {
         return view('auth.edit', ['user' => Auth::user() ]);
     }
+
     //userデータの保存
     public function update(Request $request) {
 
         $user_form = $request->all();
+        // ゲストユーザーは変更できずに即リダイレクト
         $user = Auth::user();
+        if($user->id == 1){
+            return redirect('edit');
+        }
+
         //不要な「_token」の削除
         unset($user_form['_token']);
         //保存
