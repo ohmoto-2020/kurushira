9cc6304a8134f42adf5655a4b308aaac352fa501
おーもと <vitamin29290209@gmail.com>
2021-02-06 18:35:34 +0900

    [update]履歴がない場合の処理

--- laravel/resources/views/auth/my_page.blade.php
+++ laravel/resources/views/auth/my_page.blade.php
@@ -5,25 +5,29 @@
 <div class="container">
   <p style="color:red;">{{ Auth::user()->name }}さん</p>
 
-  <a href="{{ route('edit') }}" style="color:red;">ユーザー登録内容の変更</a>
+  <a href="{{ route('edit') }}" style="color:blue;">ユーザー登録内容の変更</a>
   <p style="color:red;">前回の履歴</p>
-  <p style="color:red;">{{ $match_car['updated_at'] }}</p>
-  <ul style="color: red;">
-    <li>style:{{ $match_car['style'] }}</li>
-    <li>size:{{ $match_car['size'] }}</li>
-    <li>country:{{ $match_car['country'] }}</li>
-    <li>uses:{{ $match_car['uses'] }}</li>
-  </ul>
-  @if($match_car["match_car"]->isEmpty())
-    <p class="not-find" style="color: red;">該当する車は見つかりませんでした</p>
+  @if(!is_null($match_car))
+    <p style="color:red;">{{ $match_car['updated_at'] }}</p>
+    <ul style="color: red;">
+      <li>style:{{ $match_car['style'] }}</li>
+      <li>size:{{ $match_car['size'] }}</li>
+      <li>country:{{ $match_car['country'] }}</li>
+      <li>uses:{{ $match_car['uses'] }}</li>
+    </ul>
+    @if($match_car["match_car"]->isEmpty())
+      <p class="not-find" style="color: red;">該当する車は見つかりませんでした</p>
+    @else
+      @foreach($match_car["match_car"] as $car)
+      <div class="result__box" style="color: red;">
+        <p>車種名:{{ $car['name'] }}</p>
+        <p>メーカー:{{ $car['maker'] }}</p>
+        <p>価格:{{ $car['price'] }}万円</p>
+      </div>
+      @endforeach
+    @endif
   @else
-    @foreach($match_car["match_car"] as $car)
-    <div class="result__box" style="color: red;">
-      <p>車種名:{{ $car['name'] }}</p>
-      <p>メーカー:{{ $car['maker'] }}</p>
-      <p>価格:{{ $car['price'] }}万円</p>
-    </div>
-    @endforeach
+    <p style="color:red;">履歴はありません</p>
   @endif
 </div>
 @endsection
