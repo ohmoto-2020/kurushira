328bcecc9a03027e0c4ef553953b1c1d32e18da2
おーもと <vitamin29290209@gmail.com>
2021-02-07 17:35:14 +0900

    [css]offerページのUI

--- laravel/resources/views/page/offer.blade.php
+++ laravel/resources/views/page/offer.blade.php
@@ -2,12 +2,15 @@
 @section('title','画像投稿 | クルシラ')
 <body class="body">
 @section('content')
-<div class="section">
-  <div class="section__card">
-    <div class="section__card__header">ご提供ありがとうございます</div>
-    <div class="section__card__body">
-      <div>{{ $car_name[0]['name'] }}</div>
-      <img src="{{ $cars[0]['image'] }}"  alt="{{ $car_name[0]['name'] }}">
+<div class="offer-container">
+  <div class="offer-container__card">
+    <div class="offer-container__card__header">ご提供ありがとうございます</div>
+    <div class="offer-container__card__body">
+      <div class="offer-container__card__body__name">
+        <p>メーカー：{{ $car_name[0]['maker'] }}</p>
+        <p>&emsp;車&ensp;種&thinsp;：{{ $car_name[0]['name'] }}</p>
+      </div>
+      <img  class="offer-container__card__body__image" src="{{ $cars[0]['image'] }}"  alt="{{ $car_name[0]['name'] }}">
     </div>
   </div>
 </div>
