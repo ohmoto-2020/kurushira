9ee056ffa437de9d3b695ab8fd4e13a0755080d4
おーもと <vitamin29290209@gmail.com>
2021-02-12 17:53:33 +0900

    [change]carsテーブルと紐付け

--- laravel/database/migrations/2021_01_29_221351_create_car_images_table.php
+++ laravel/database/migrations/2021_01_29_221351_create_car_images_table.php
@@ -14,7 +14,7 @@ class CreateCarImagesTable extends Migration
     public function up()
     {
         Schema::create('car_images', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
             $table->bigInteger('car_id')->unsigned();
             $table->text('image');
             $table->timestamps();
@@ -23,6 +23,11 @@ class CreateCarImagesTable extends Migration
             $table->foreign('car_id')
                     ->references('id')
                     ->on('cars')
+                    ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
                     ->onDelete('cascade');
         });
     }
