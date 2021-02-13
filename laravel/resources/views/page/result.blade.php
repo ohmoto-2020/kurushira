222e4e7f3ea51f053f5f8d5b5dfa5f8228eef645
おーもと <vitamin29290209@gmail.com>
2021-02-13 17:58:15 +0900

    [css]twitterシェアボタンの配置変更

--- laravel/resources/views/page/result.blade.php
+++ laravel/resources/views/page/result.blade.php
@@ -7,8 +7,10 @@
   @if(empty($match_cars->toArray()['data']))
     <p class="not-find">該当する車は見つかりませんでした</p>
   @else
-  <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false" data-size="large" data-text="自分に合った車">Tweet</a>
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <div class="twitter">
+      <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false" data-size="large" data-text="自分に合った車">Tweet</a>
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
     @include('match')
   @endif
 
