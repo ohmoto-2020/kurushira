08b6a1320955636c1c7ff0996276cd40ec13ebc2
おーもと <vitamin29290209@gmail.com>
2021-02-18 14:26:53 +0900

    [fix]https接続時CSS反映

--- laravel/resources/views/common.blade.php
+++ laravel/resources/views/common.blade.php
@@ -5,11 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- CSS -->
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    @if(app('env')=='local')
+      <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    @elseif(app('env')=='production')
+      <link href="{{ secure_asset('/css/app.css') }}" rel="stylesheet">
+    @endif
+
     <!-- font awesome -->
     <script src="https://kit.fontawesome.com/2353a33daa.js" crossorigin="anonymous"></script>
     <!-- ファビコン -->
-    <link rel="icon" href="{{ asset('/favicon.ico') }}">
+    @if(app('env')=='local')
+      <link rel="icon" href="{{ asset('/favicon.ico') }}">
+    @elseif(app('env')=='production')
+      <link rel="icon" href="{{ secure_asset('/favicon.ico') }}">
+    @endif
     <!-- swiper.js -->
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
@@ -23,7 +32,7 @@
       @include('layouts.header')
     </header>
 
-    <main style="margin-top:2rem;">
+    <main style="margin-top:6rem;">
       @yield('content')
     </main>
 
