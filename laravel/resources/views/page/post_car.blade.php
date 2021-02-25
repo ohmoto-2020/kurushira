490047b1515c9e23edcc04694b9e93f4571f24e2
おーもと <vitamin29290209@gmail.com>
2021-02-25 22:53:59 +0900

    [change]ゲストも画像提供可能

--- laravel/resources/views/page/post_car.blade.php
+++ laravel/resources/views/page/post_car.blade.php
@@ -4,9 +4,6 @@
 @section('content')
 <div class="section">
   <p style="color: red;">{{ session('message') }}</p>
-  @if(Auth::id() == 1)
-    <p style="color:red;">ゲストユーザーは画像提供ができません</p>
-  @endif
   <p class="section__title">2ステップで画像を提供する<br><span>※約1分で操作が完了します</span></p>
   <form action="{{ action('CarController@post_car') }}" class="section__form" method="POST" enctype="multipart/form-data">
     @csrf
@@ -17,27 +14,16 @@
         <div class="section__form__card__maker__box">
           <div class="section__form__card__maker__box__form">
             <label>
-            @if(Auth::id() == 1)
-              <input type="radio" name="maker" value="{{ $maker }}" disabled>
-              {{ $maker }}
-            @else
               <input type="radio" name="maker" value="{{ $maker }}">
               {{ $maker }}
-            @endif
             </label>
           </div>
           <select name="{{ $maker }}">
             <option value="">---------</option>
             @foreach($cars as $car)
-            @if(Auth::id() == 1)
-              <option value="{{ $car['name'] }}" disabled>
-                {{ $car['name'] }}
-              </option>
-            @else
               <option value="{{ $car['name'] }}">
                 {{ $car['name'] }}
               </option>
-            @endif
             @endforeach
           </select>
         </div>
@@ -48,19 +34,11 @@
     <div class="section__form__card">
       <p class="section__form__card__header"><i class="fas fa-camera"></i>STEP2<span>画像を選択してください</span></p>
       <div class="section__form__card__file">
-        @if(Auth::id() == 1)
-          <input type="file" name="file" disabled>
-        @else
-          <input type="file" name="file" accept="image/*">
-        @endif
+        <input type="file" name="file" accept="image/*">
       </div>
     </div>
     <div class="section__form__card__button">
-      @if(Auth::id() == 1)
-        <input type="submit" value="アップロード" class="send" disabled>
-      @else
-        <input type="submit" value="アップロード" class="send">
-      @endif
+      <input type="submit" value="アップロード" class="send">
     </div>
   </form>
 </div>
