c963a420452309404e8ac1ce1c3033b1cc2f3410
おーもと <vitamin29290209@gmail.com>
2021-04-18 12:30:46 +0900

    [add]通報テーブル

---
+++ laravel/database/migrations/2021_04_16_145454_create_reports_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('image_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('image_id')
+                ->references('id')
+                ->on('car_images')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
